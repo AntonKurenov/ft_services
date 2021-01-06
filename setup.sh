7f5c6cd09240abf4c49f878cba0f84e335c00734
Emmaline Lovegood <elovegoo@si-o2.kzn.21-school.ru>
2021-01-06 18:57:18 +0300

    Final

--- setup.sh
+++ setup.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+minikube start --vm-driver=virtualbox
+
 eval $(minikube docker-env)
 
 docker build -t nginx_image ./srcs/nginx/
 docker build -t phpmyad_image ./srcs/phpmyad/
-docker build -t mysql_image ./srcs/mysql
-docker build -t wordpress_image ./srcs/wordpress
-docker build -t grafana_image ./srcs/grafana
-docker build -t ftps_image ./srcs/ftps
-docker build -t influxdb_image ./srcs/influxdb
+docker build -t mysql_image ./srcs/mysql/
+docker build -t wordpress_image ./srcs/wordpress/
+docker build -t grafana_image ./srcs/grafana/
+docker build -t ftps_image ./srcs/ftps/
+docker build -t influxdb_image ./srcs/influxdb/
 
 minikube addons enable metallb
-kubectl apply -f configmap.yaml
+kubectl apply -f ./srcs/configmap.yaml
 
 kubectl apply -f ./srcs/volumes/persistent_vol.yaml
 
@@ -23,4 +25,5 @@ kubectl apply -f ./srcs/influxdb/influxdb.yaml
 kubectl apply -f ./srcs/mysql/mysql.yaml
 kubectl apply -f ./srcs/grafana/grafana.yaml
 
-# minikube dashboard
+ssh-keygen -R 192.168.99.101
+minikube dashboard
